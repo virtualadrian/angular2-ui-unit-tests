85ba7d77a9b08548103bf05b879424189aa9c621
Adrian Pavelescu <virtualadrian@users.noreply.github.com>
2016-11-09 12:28:02 -0800

    Unit Test Grab Bag: Swiss Army Knife of Examples
    Found in another plunk that was also not linked to or documented.

---
+++ grab-bag-unit-tests/systemjs.config.extras.js
@@ -0,0 +1,15 @@
+/** App specific SystemJS configuration */
+System.config({
+  packages: {
+    // barrels
+    'app/model': {main:'index.js', defaultExtension:'js'},
+    'app/model/testing': {main:'index.js', defaultExtension:'js'}
+  }
+});
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
